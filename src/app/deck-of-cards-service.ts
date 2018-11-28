a47bfc926b75b7972827a9efe7bcdfeeaec09ccf
Ysael Pepin <me@ysael.com>
2018-11-28 10:22:50 -0500

    refactor

--- src/app/deck-of-cards-service.ts
+++ src/app/deck-of-cards-service.ts
@@ -1,62 +1,89 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import produce from "immer"
+import { Injectable } from '@angular/core'
+import { BehaviorSubject, Observable } from 'rxjs'
+import produce from 'immer'
 interface Card {
-  value: string;
-  suit: string;
-  isSeleCted: boolean;
+    value: string
+    suit: string
 }
 
-export type Deck = Card[];
+export type Deck = Card[]
+export type Game = {
+    deck: Deck
+    selectedCards: Deck
+}
 
-type Suits = string[];
-type Values = string[];
+type Suits = string[]
+type Values = string[]
 
-const numbers = ['two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
-const initialSuits: Suits = ['clubs', 'diamonds', 'hearts', 'spades'];
-const InitialValues = ['ace', ...numbers, 'jack', 'queen', 'king'];
+const numbers = [
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+]
+const initialSuits: Suits = ['clubs', 'diamonds', 'hearts', 'spades']
+const InitialValues = ['ace', ...numbers, 'jack', 'queen', 'king']
 
-const makeACard = (value: string, suit: string): Card => ({ value, suit, isSeleCted: false });
-const makeASuit = (values: Values, suit: string) => values.map(value => makeACard(value, suit));
-const deckReducer = (values: Values) => (d, suit) => [...d, ...makeASuit(values, suit)];
-const createDeck = (suits: Suits, values: Values): Deck => suits.reduce(deckReducer(values), []);
+const makeACard = (value: string, suit: string): Card => ({ value, suit })
+const makeASuit = (values: Values, suit: string) =>
+    values.map(value => makeACard(value, suit))
+const deckReducer = (values: Values) => (d, suit) => [
+    ...d,
+    ...makeASuit(values, suit),
+]
+const createDeck = (suits: Suits, values: Values): Deck =>
+    suits.reduce(deckReducer(values), [])
 
-const shuffleDeck = d => d
-  .map(a => [Math.random(), a])
-  .sort((a, b) => a[0] - b[0])
-  .map(a => a[1]);
+const shuffleDeck = d =>
+    d
+        .map(a => [Math.random(), a])
+        .sort((a, b) => a[0] - b[0])
+        .map(a => a[1])
 
-const randomCardIndex = d => Math.floor(Math.random() * d.length);
+const randomCardIndex = d => Math.floor(Math.random() * d.length)
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
-
 export class DeckOfCardsService {
-  deck: Deck;
+    game: Game
 
-  private deckSource = new BehaviorSubject<Deck>(createDeck(initialSuits, InitialValues));
-  decks = this.deckSource.asObservable();
-  selectedCardIndex: number;
+    private gameSource = new BehaviorSubject<Game>({
+        deck: createDeck(initialSuits, InitialValues),
+        selectedCards: [],
+    })
+    gameObservable = this.gameSource.asObservable()
+    selectedCardIndex: number
 
-  constructor() {
-    this.selectedCardIndex = null;
-    this.decks.subscribe(d => this.deck = d);
-  }
+    constructor() {
+        this.selectedCardIndex = null
+        this.gameObservable.subscribe(g => (this.game = g))
+    }
 
-  getDeck() {
-    return this.decks;
-  }
+    getGame() {
+        return this.gameObservable
+    }
 
-  suffle() {
-    this.deckSource.next(shuffleDeck(this.deck));
-  }
+    suffle() {
+        this.gameSource.next({
+            deck: shuffleDeck(this.game.deck),
+            selectedCards: this.game.selectedCards,
+        })
+    }
 
-  selectACard() {
-    this.deckSource.next(
-      produce(this.deck, draftDeck => {
-        draftDeck.forEach(d => d.isSeleCted = false);
-        draftDeck[randomCardIndex(this.deck)].isSeleCted = true
-      })
-    );
-  }
+    selectACard() {
+        this.gameSource.next(
+            produce(this.game, draftGame => {
+                const selectedCard = draftGame.deck.splice(
+                    randomCardIndex(this.game.deck),
+                    1
+                )[0]
+                draftGame.selectedCards.push(selectedCard)
+            })
+        )
+    }
 }
--- src/app/deck/deck.component.ts
+++ src/app/deck/deck.component.ts
@@ -1,28 +1,29 @@
-import { DeckOfCardsService, Deck } from './../deck-of-cards-service';
-import { Component, OnInit } from '@angular/core';
-import { CardsSymbols } from './cards-symbols';
+import { DeckOfCardsService, Deck } from './../deck-of-cards-service'
+import { Component, OnInit } from '@angular/core'
+import { CardsSymbols } from './cards-symbols'
 
-const setColSize = innerWidth => (window.innerWidth <= 1000) ? 4 : 6
+const setColSize = innerWidth => (window.innerWidth <= 1000 ? 4 : 6)
 @Component({
-  selector: 'app-deck',
-  templateUrl: './deck.component.html',
-  styleUrls: ['./deck.component.sass']
+    selector: 'app-deck',
+    templateUrl: './deck.component.html',
+    styleUrls: ['./deck.component.sass'],
 })
 export class DeckComponent implements OnInit {
-  deck: Deck;
-  cards = CardsSymbols;
-  breakpoint: number;
-  constructor(private deckOfCardsService: DeckOfCardsService) { }
+    deck: Deck
+    selectedCards: Deck
+    cards = CardsSymbols
+    breakpoint: number
+    constructor(private deckOfCardsService: DeckOfCardsService) {}
 
-  ngOnInit() {
-    this.breakpoint = setColSize(window.innerWidth);
-    this.deckOfCardsService.getDeck().subscribe(d => this.deck = d);
-  }
+    ngOnInit() {
+        this.breakpoint = setColSize(window.innerWidth)
+        this.deckOfCardsService.getGame().subscribe(g => {
+            this.deck = g.deck
+            this.selectedCards = g.selectedCards
+        })
+    }
 
-  onResize(event) {
-    this.breakpoint = setColSize(event.target.innerWidth);
-  }
+    onResize(event) {
+        this.breakpoint = setColSize(event.target.innerWidth)
+    }
 }
-
-
-
